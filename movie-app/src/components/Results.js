835d1fccdc9e01b2cc67f39ba7e3c85731948e01
David <digital0david@gmail.com>
2021-01-08 22:41:42 -0500

    added styled components / partial nominate feature

--- movie-app/src/components/Results.js
+++ movie-app/src/components/Results.js
@@ -1,37 +1,62 @@
 import React, { useState, useEffect } from "react";
 import "../App.css";
 
+import styled from "styled-components";
+
+const Container = styled.div`
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+`;
+
 function Results(props) {
   const [movies, setMovies] = useState([]);
+  const [nominate, setNominate] = useState([]);
 
   useEffect(() => {
-    if (movies.length > 5) {
+    if (movies.length >= 5) {
       movies.shift();
     }
+    setNominate(nominate.concat(props.movie), [nominate]);
+
     setMovies(movies.concat(props.movie));
   }, [props.movie]);
+
+  const nominateMovie = () => {
+    if (nominate.length > 5) {
+      return alert("Too many movies!");
+    } else {
+      setNominate(nominate.concat(props.movie));
+      console.log(nominate);
+    }
+  };
 
   const clearMovies = () => {
     setMovies([]);
   };
 
   return (
-    <div className="results">
-      {movies ? (
-        movies.map((movie) => {
-          return (
-            <div>
-              <h3>{movie.Title}</h3>
-              <p>{movie.Year}</p>
-              <img src={movie.Poster} alt="Poster" />
-            </div>
-          );
-        })
-      ) : (
-        <p>"Loading"</p>
-      )}
+    <Container>
+      <div className="results">
+        {movies ? (
+          movies.map((movie) => {
+            return (
+              <div key={movie.imdbID} className="movie">
+                <h3>{movie.Title}</h3>
+                <p>{movie.Year}</p>
+                <button onClick={nominateMovie}>Nominate</button>
+              </div>
+            );
+          })
+        ) : (
+          <p>"Loading"</p>
+        )}
+      </div>
       <button onClick={clearMovies}>Clear</button>
-    </div>
+    </Container>
   );
 }
 
