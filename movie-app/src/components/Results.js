032234fd88dd6b2855a664fb0fff57fd9e2fc4c7
David <digital0david@gmail.com>
2021-01-12 23:36:05 -0500

    moved styles to component/refactoring search results

--- movie-app/src/components/Results.js
+++ movie-app/src/components/Results.js
@@ -12,25 +12,59 @@ const Container = styled.div`
   width: 100%;
 `;
 
+const Poster = styled.img`
+  width: 50%;
+  border: 1px solid black;
+`;
+
+const Movie = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  align-content: flex-start;
+  width: 20%;
+  border: 1px solid black;
+  background-color: rgb(102, 102, 104);
+  padding: 2%;
+  margin: 1%;
+`;
+
+const MovieTitle = styled.h3`
+  color: gold;
+`;
+
 function Results(props) {
   const [movies, setMovies] = useState([]);
   const [nominate, setNominate] = useState([]);
 
-  useEffect(() => {
-    if (movies.length >= 5) {
-      movies.shift();
-    }
-    setNominate(nominate.concat(props.movie), [nominate]);
+  useEffect(
+    () => {
+      // if (movies.length >= 5) {
+      //   movies.shift();
+      // }
 
-    setMovies(movies.concat(props.movie));
-  }, [props.movie]);
+      // for (let i = 0; i < movies.length; i++) {
+      //   if (movies[i].Title === props.movie.Title) {
+      //     return alert("Already searched");
+      //   }
+      // }
+      console.log(props.movie);
+      // if (props.movie.length !== 0) {
+      //   for (let i = 0; i < props.movie["Search"].length; i++) {
+      //     setMovies((movies[i] = props.movie["Search"][i]));
+      //     console.log("hello", movies[i].Title, movies[i].Year);
+      //   }
+      // }
+    },
+    [props.movie],
+    [movies]
+  );
 
   const nominateMovie = () => {
-    if (nominate.length > 5) {
+    if (nominate.length > 4) {
       return alert("Too many movies!");
     } else {
       setNominate(nominate.concat(props.movie));
-      console.log(nominate);
     }
   };
 
@@ -40,21 +74,23 @@ function Results(props) {
 
   return (
     <Container>
-      <div className="results">
+      {/* <div className="results">
         {movies ? (
           movies.map((movie) => {
             return (
-              <div key={movie.imdbID} className="movie">
-                <h3>{movie.Title}</h3>
+              <Movie key={movie.imdbID}>
+                <MovieTitle>{movie.Title}</MovieTitle>
                 <p>{movie.Year}</p>
+                <p>{movie.Plot}</p>
+                <Poster src={movie.Poster} alt="Poster" />
                 <button onClick={nominateMovie}>Nominate</button>
-              </div>
+              </Movie>
             );
           })
         ) : (
           <p>"Loading"</p>
         )}
-      </div>
+      </div> */}
       <button onClick={clearMovies}>Clear</button>
     </Container>
   );
